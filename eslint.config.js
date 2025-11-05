6ba6e1252a17142a92e5cf95518ea18bbda78e5f
unix-chewy <sunga.joney22@gmail.com>
2025-11-05 13:53:57 +0800

    Fix ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,18 @@ module.exports = [
         files: ["*.js"],
         languageOptions: {
             ecmaVersion: 2022,
-            sourceType: "module"
+            sourceType: "script",  // Changed from "module" to "script"
+            globals: {
+                window: "readonly",
+                document: "readonly",
+                console: "readonly",
+                module: "readonly",
+                require: "readonly"
+            }
         },
         rules: {
             "semi": ["error", "always"],
-            "no-unused-vars": "warn",
+            "no-unused-vars": "warn", 
             "no-console": "warn",
             "indent": ["error", 4],
             "quotes": ["error", "single"],
